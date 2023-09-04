e9a306bd31d075fe3c05582162ccc94c98cbb554
Jithin Palepu <44967770+jpalepu@users.noreply.github.com>
2023-09-04 09:11:16 +0200

    final version of the test file for the RAK module

--- main/rakTest.c
+++ main/rakTest.c
@@ -6,11 +6,13 @@
 #include "driver/gpio.h"
 #include "string.h"
 
-#define UART_NUM UART_NUM_1
-#define TXD_PIN GPIO_NUM_1
-#define RXD_PIN GPIO_NUM_3
+#define UART_NUM UART_NUM_2
+//UART 2
+#define TXD_PIN GPIO_NUM_17
+#define RXD_PIN GPIO_NUM_16
 #define BUF_SIZE (1024)
-#define COMMAND_QUEUE_SIZE 10
+#define COMMAND_QUEUE_SIZE 5
+#define MAX_QUEUE_MSG_SIZE 128
 
 QueueHandle_t command_queue;
 
@@ -35,14 +37,16 @@ void send_at_command(const char* command) {
 }
 
 void at_command_task(void* pvParameter) {
-     char uart_data[BUF_SIZE]; 
+    char uart_data[BUF_SIZE]; 
     while (1) {
-        char* command;
+        char command[MAX_QUEUE_MSG_SIZE];
         if (xQueueReceive(command_queue, &command, portMAX_DELAY)) {
+            printf("Received a command: %s", command);
+            
             send_at_command(command);
             vTaskDelay(pdMS_TO_TICKS(1000)); 
 
-            int len = uart_read_bytes(UART_NUM, uart_data, BUF_SIZE, pdMS_TO_TICKS(5000)); 
+            int len = uart_read_bytes(UART_NUM, uart_data, BUF_SIZE, pdMS_TO_TICKS(5000)); // Adjust timeout as needed
             if (len > 0) {
                 uart_data[len] = '\0';
                 printf("Response: %s\n", uart_data);
@@ -53,7 +57,7 @@ void at_command_task(void* pvParameter) {
 
 void app_main() {
     uart_init();
-    command_queue = xQueueCreate(COMMAND_QUEUE_SIZE, sizeof(char*));
+    command_queue = xQueueCreate(COMMAND_QUEUE_SIZE, MAX_QUEUE_MSG_SIZE);
 
     xTaskCreate(at_command_task, "at_command_task", 2048, NULL, 5, NULL);
 
@@ -61,13 +65,16 @@ void app_main() {
     while (1) {
         const char* at_commands[] = {
             "AT\r\n",
-            "AT+GMR\r\n",
+            "AT+VER=?\r\n",
             // we can add more and mroe at commands here. 
         };
 
+        char tx_buffer[MAX_QUEUE_MSG_SIZE];
+
         for (int i = 0; i < sizeof(at_commands) / sizeof(at_commands[0]); i++) {
-            char* command = strdup(at_commands[i]);
-            xQueueSend(command_queue, &command, portMAX_DELAY);
+            sprintf(tx_buffer, &(at_commands[i][0]));
+            //char* command = strdup(at_commands[i]);
+            xQueueSend(command_queue, (void *)tx_buffer, portMAX_DELAY);
         }
 
         // delay for certain time interval before sending commands again
